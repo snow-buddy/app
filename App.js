c20484e58c929cf491dac9feb9ff3c72af570338
Mirjam Skarica <mirjam.skarica@booking.com>
2019-04-08 14:51:13 +0200

    Add two buttons

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Button, Alert, TouchableHighlight} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,19 +18,38 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-       <Image
-          style={{width: 220, height: 158}}
-          source={require('./src/res/img/logo.png')}
-        />
-        <Text style={styles.welcome}>Welcome to Snow Buddy!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    style={{width: 220, height: 158}}
+                    source={require('./src/res/img/logo.png')}
+                />
+                <Text style={styles.welcome}>Welcome to Snow Buddy!</Text>
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
+                <View style={styles.button}>
+                    <TouchableHighlight style={styles.buttonText}
+                        onPress={this._onPressButton}
+                        underlayColor="blue" >
+                      <Text> Register </Text>
+                    </TouchableHighlight>
+
+                    <TouchableHighlight style={styles.buttonText}
+                                        onPress={this._onPressButton}
+                                        underlayColor="blue" >
+                        <Text> Login </Text>
+                    </TouchableHighlight>
+                </View>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -50,4 +69,19 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+
+   button: {
+       marginBottom: 30,
+       width: 260,
+       alignItems: 'center'
+
+   },
+   buttonText: {
+       alignItems: 'center',
+       backgroundColor: '#2196F3',
+       width: 260,
+       padding:20,
+       marginBottom: 10
+
+   }
 });
